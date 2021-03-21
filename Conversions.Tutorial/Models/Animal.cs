9d5eb2b147022ba970c704ebabd69463fe7ef51b
sshimabuku <sashi.shimabuku@gmail.com>
2021-03-21 10:03:07 -0400

    Implement Conversions tutorial

---
+++ Conversions.Tutorial/Models/Animal.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conversions.Tutorial.Models
+{
+    public class Animal { }
+
+    public class Dog:Animal { }
+
+    public class Cat:Animal {}
+}
--- Conversions.Tutorial/Program.cs
+++ Conversions.Tutorial/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Conversions.Tutorial.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,84 @@ namespace Conversions.Tutorial
     {
         static void Main(string[] args)
         {
+            #region Implicit Conversions
+
+            int lessPrecise = 10;
+            double morePrecise = lessPrecise;
+
+            Console.WriteLine($"Integer: {lessPrecise}"); // Integer: 10
+            Console.WriteLine($"Double: {morePrecise}"); // Double: 10
+            Console.WriteLine();
+
+
+            Dog dog = new Dog();
+            Animal animal = dog;
+
+            Console.WriteLine($"Dog: {dog}"); // Dog: Conversions.Tutorial.Models.Dog
+            Console.WriteLine($"Animal: {animal}"); // Animal: Conversions.Tutorial.Models.Dog
+            Console.WriteLine();
+
+            #endregion
+
+            #region Explicit Conversions
+
+            double morePreciseNum = 15.35;
+
+            // The line below will throw an error
+            //int lessPreciseNum = preciseNum;
+
+            // By adding a cast, the program will compile
+            int lessPreciseNum = (int)morePreciseNum;
+
+            Console.WriteLine($"Double: {morePreciseNum}"); // Double: 15.35
+            Console.WriteLine($"Integer: {lessPreciseNum}"); // Integer: 15
+            Console.WriteLine();
+
+            Dog d = new Dog();
+            Animal animalParent = d;
+
+            // The line above will throw an error
+            // Dog dogChild = animalParent;
+
+
+            // By adding a cast, the program will compile
+            Dog dogChild = (Dog)animalParent;
+
+            Console.WriteLine($"Dog d: {animalParent}"); // Animal: Conversions.Tutorial.Models.Dog
+            Console.WriteLine($"Animal animalParent: {animalParent}"); // Animal animalParent: Conversions.Tutorial.Models.Dog
+            Console.WriteLine($"Dog dogChild: {dogChild}"); // Dog dogChild: Conversions.Tutorial.Models.Dog
+            Console.WriteLine();
+
+            Cat cat = new Cat();
+            Animal catAnimal = cat;
+
+            try
+            {
+                Dog catDog = (Dog)catAnimal;
+                Console.WriteLine(catDog);
+            }
+            catch (InvalidCastException ex)
+            {
+                // Unable to cast object of type 'Conversions.Tutorial.Models.Cat' 
+                //to type 'Conversions.Tutorial.Models.Dog'
+                Console.WriteLine(ex.Message);
+                Console.WriteLine();
+            }
+
+            #endregion
+
+            #region Conversions with helper classes
+
+            string numberText = "123.45";
+            double numberConvert = Convert.ToDouble(numberText);
+            double numberParse = double.Parse(numberText);
+
+            Console.WriteLine($"Using Convert: {numberConvert}"); // Using Convert: 123.45
+            Console.WriteLine($"Using Parse: {numberParse}"); // Using Parse: 123.45
+
+            #endregion
+
+            Console.ReadKey();
         }
     }
 }
